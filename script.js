9b39fe765c6cc50c14d2818e8f5a32c5f0b1d336
Miles Rigby <milesjamesrigby@gmail.com>
2025-09-24 11:50:01 +0100

    Added comments to JS, and default case in operate() function for error checking

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let firstOperand;
 let secondOperand;
 let operator;
 
+// operate takes in two operands, a and b, and performs an operation on them
+// operator is a string representing one of four operations -
+// addition, subtraction, multiplication, and division: "+", "-", "*", "/"
 function operate(a, operator, b) {
     switch(operator){
         case "+":
@@ -12,21 +15,14 @@ function operate(a, operator, b) {
             return mult(a, b);
         case "/":
             return div(a, b);
+        default:
+            console.log("ERROR: INVALID OPERATOR IN operate()")
     }
 }
 
-function add(a, b){
-    return a+b;
-}
 
-function sub(a, b){
-    return a-b;
-}
-
-function mult(a, b){
-    return a*b;
-}
-
-function div(a, b){
-    return a/b;
-}
+// Simple arithmetic operations. a - first operand, b - second operand.
+function add(a, b){ return a+b; }
+function sub(a, b){ return a-b; }
+function mult(a, b){ return a*b; }
+function div(a, b){ return a/b; }
